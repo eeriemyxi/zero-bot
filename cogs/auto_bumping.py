745451dfc5c19f3f53ca917e1216618a64f28ccd
Myxi <myxi@duck.com>
2021-12-19 00:23:30 +0530

    auto bumping

---
+++ cogs/auto_bumping.py
@@ -0,0 +1,36 @@
+"""
+This one is against the TOS. It uses a different bot's messages because I am lazy.
+"""
+from os import getenv
+
+import disnake
+import httpx
+from disnake.ext import commands
+
+
+class AutoBump(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.reminder_bot_id = 735147814878969968
+        self.role_id = int(getenv("BUMP_REMINDER_ROLE_ID"))
+        self.channel_id = int(getenv("AUTO_BUMP_CHANNEL_ID"))
+        self.token = getenv("AUTO_BUMP_USER_TOKEN")
+        self.url = "https://canary.discord.com/api/v9/channels/%s/messages"
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if (message.channel.id, message.author.id) == (
+            self.channel_id,
+            self.reminder_bot_id,
+        ):
+            if any(self.role_id == role.id for role in message.role_mentions):
+                async with httpx.AsyncClient() as session:
+                    await session.post(
+                        url=self.url % self.channel_id,
+                        headers=dict(authorization=self.token),
+                        json={"content": "!d bump", "content-type": "application/json"},
+                    )
+
+
+def setup(bot):
+    bot.add_cog(AutoBump(bot))
