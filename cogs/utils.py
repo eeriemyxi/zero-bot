3a515fd5b6ae39da2d6c5a63df022b9fdecb1263
Myxi <myxi@duck.com>
2022-01-07 14:30:38 +0530

    fix

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,8 +1,10 @@
+from asyncio import sleep
+from contextlib import suppress
+from os import environ
+
 import disnake
 from disnake.ext import commands
-from ext.utils import send_message, parse_env_data
-from os import environ
-from contextlib import suppress
+from ext.utils import parse_env_data, send_message
 
 
 class Utils(commands.Cog):
@@ -10,15 +12,17 @@ class Utils(commands.Cog):
         self.bot = bot
         self.tokens = parse_env_data(environ.get("USER_TOKENS"))
     
-    @commands.command(aliases=["up"])
-    async def userphone(self, ctx):
-        with suppress(Exception):
-            await send_message(ctx.bot.session, ctx.channel.id, self.tokens["dankruptbest"], "--userphone")
-    
     @commands.is_owner()
     @commands.command()
     async def send(self, ctx, user: str, *, content: str):
+        if user.lower() == "all":
+            for username in self.tokens:
+                with suppress(Exception):
+                    await send_message(self.bot.session, ctx.channel.id, self.tokens[username], content)
+                    await sleep(2)
+            return
+
         for username in self.tokens:
-            if username.startswith(user):
+            if username.startswith(user.lower()):
                 with suppress(Exception):
                     await send_message(self.bot.session, ctx.channel.id, self.tokens[username], content)
