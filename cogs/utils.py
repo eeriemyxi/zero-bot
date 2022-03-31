77eedb493ca81235117e90c9bf39dd014c717808
Myxi <myxi@duck.com>
2022-03-31 21:06:53 +0530

    ping active members feature

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,54 +1,74 @@
+import threading
+import time
 from asyncio import sleep
+from collections import UserList
 from contextlib import suppress
+from dataclasses import dataclass
 from os import environ
 
 import disnake
 from disnake.ext import commands
+from ext.constants import PREFIXES
 from ext.utils import parse_env_data, send_message
 
 
-PREFIXES = (
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "(",
-    ")",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ";",
-    "=",
-    ">",
-    "?",
-    "[",
-    "]",
-    "^",
-    "{",
-    "|",
-    "}",
-    "~",
-    "₹",
-    "ch!",
-    "m!",
-    "s.",
-    "bday",
-    "pls",
-    "owo",
-)
+@dataclass
+class ActiveLoggingMessage:
+    author: int
+    time: float
+
+
+class ActiveUserLogging(UserList):
+    def __init__(self, expiry_time: int):
+        super().__init__(self)
+        self.data: list[ActiveLoggingMessage]
+        self.expiry = expiry_time
+
+    def __contains__(self, msg: disnake.Message) -> bool:
+        for m in self.data:
+            if msg.author.id == m.author:
+                return True
+
+        return False
+
+    def add_user(self, msg: disnake.Message):
+        log_msg = ActiveLoggingMessage(
+            author=msg.author.id, time=int(msg.created_at.timestamp())
+        )
+
+        if msg not in self:
+            super().append(log_msg)
+        else:
+            for i, x in enumerate(self.data.copy()):
+                if x.author == log_msg.author:
+                    del self.data[i]
+            super().append(log_msg)
+
+        threading.Thread(None, self.expire, args=[log_msg]).start()
+
+    def expire(self, msg: ActiveLoggingMessage):
+        before_time = msg.time
+        time.sleep(self.expiry)
+
+        for i, m in enumerate(self.data):
+            if msg.author == m.author and before_time <= m.time:
+                del self.data[i]
+
+    def get_users_for(self, seconds: int):
+        for user in self.data:
+            if time.time() - user.time <= seconds:
+                yield user.author
 
 
 class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tokens = parse_env_data(environ.get("USER_TOKENS"))
+        self.active_users = ActiveUserLogging(expiry_time=18000)
 
     @commands.is_owner()
     @commands.command()
-    async def send(self, ctx, user: str, *, content: str):
+    async def send(self, ctx: commands.Context, user: str, *, content: str):
         if user.lower() == "all":
             for username in self.tokens:
                 with suppress(Exception):
@@ -107,3 +127,29 @@ class Utils(commands.Cog):
 
         with suppress(Exception):
             await ctx.message.delete()
+
+    @commands.Cog.listener("on_message")
+    async def logging(self, msg: disnake.Message):
+        if not msg.author.bot:
+            self.active_users.add_user(msg)
+
+    @commands.slash_command()
+    async def ping(
+        self,
+        inter: disnake.CommandInteraction,
+        hours: int = commands.Param(
+            choices={f"{x} hour(s) to present": x for x in range(1, 6)}
+        ),
+    ):
+        """Pings all the users who sent messages in a period of time.
+
+        Parameters
+        ----------
+        hours: The period of time.
+        """
+        await inter.send(
+            content=", ".join(
+                f"<@{x}>" for x in self.active_users.get_users_for(3600 * hours)
+            )
+            or "No users recorded yet, perhaps, it's because the chat has been dead for 5 hours?"
+        )
