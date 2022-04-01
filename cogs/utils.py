81f3f51b9a4c50369eb2700c5caaa67bca042cf2
Myxi <myxi@duck.com>
2022-04-01 19:53:42 +0530

    multi channel support and code improvements: ping active members

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,9 +1,8 @@
 import threading
 import time
 from asyncio import sleep
-from collections import UserList
 from contextlib import suppress
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from os import environ
 
 import disnake
@@ -12,59 +11,55 @@ from ext.constants import PREFIXES
 from ext.utils import parse_env_data, send_message
 
 
-@dataclass
-class ActiveLoggingMessage:
-    author: int
-    time: float
+@dataclass(unsafe_hash=True)
+class ActiveLogMessage:
+    time: int = field(compare=False, hash=False)
+    author: int = field(compare=True, hash=True)
 
 
-class ActiveUserLogging(UserList):
+class ActiveUserLogging:
     def __init__(self, expiry_time: int):
-        super().__init__(self)
-        self.data: list[ActiveLoggingMessage]
-        self.expiry = expiry_time
+        self.data: set[ActiveLogMessage] = set()
+        self.expiry_time = expiry_time
 
-    def __contains__(self, msg: disnake.Message) -> bool:
-        for m in self.data:
-            if msg.author.id == m.author:
-                return True
+    def add_user(self, msg):
+        log_msg = ActiveLogMessage(msg.created_at.timestamp(), msg.author.id)
 
-        return False
+        self.data.add(log_msg)
+        for x in self.data:
+            if x == log_msg:
+                x.time = log_msg.time
+                break
 
-    def add_user(self, msg: disnake.Message):
-        log_msg = ActiveLoggingMessage(
-            author=msg.author.id, time=int(msg.created_at.timestamp())
-        )
+        thread = threading.Thread(None, target=self.expire, args=[log_msg], daemon=True)
+        thread.start()
+    
+    def get_log_msg(self, author: int) -> ActiveLogMessage:
+        for log_msg in self.data.copy():
+            if log_msg.author == author:
+                return log_msg
 
-        if msg not in self:
-            super().append(log_msg)
-        else:
-            for i, x in enumerate(self.data.copy()):
-                if x.author == log_msg.author:
-                    del self.data[i]
-            super().append(log_msg)
+    def expire(self, log_msg: ActiveLogMessage):
+        time.sleep(self.expiry_time)
 
-        threading.Thread(None, self.expire, args=[log_msg]).start()
-
-    def expire(self, msg: ActiveLoggingMessage):
-        before_time = msg.time
-        time.sleep(self.expiry)
-
-        for i, m in enumerate(self.data):
-            if msg.author == m.author and before_time <= m.time:
-                del self.data[i]
+        for x in self.data.copy(): 
+            if (x.author, x.time) == (log_msg.author, log_msg.time):
+                self.data.remove(x)
 
     def get_users_for(self, seconds: int):
         for user in self.data:
             if time.time() - user.time <= seconds:
                 yield user.author
+    
+    def __repr__(self) -> str:
+        return repr(self.data)
 
 
 class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tokens = parse_env_data(environ.get("USER_TOKENS"))
-        self.active_users = ActiveUserLogging(expiry_time=18000)
+        self.active_users = dict()
 
     @commands.is_owner()
     @commands.command()
@@ -131,15 +126,22 @@ class Utils(commands.Cog):
     @commands.Cog.listener("on_message")
     async def logging(self, msg: disnake.Message):
         if not msg.author.bot:
-            self.active_users.add_user(msg)
+            try:
+                logs = self.active_users[msg.channel.id]
+            except KeyError:
+                logs = ActiveUserLogging(18000)
+                self.active_users[msg.channel.id] = logs
+
+            logs.add_user(msg)
+
 
     @commands.slash_command()
     async def ping(
         self,
         inter: disnake.CommandInteraction,
+        channel: disnake.TextChannel,
         hours: int = commands.Param(
-            choices={f"{x} hour(s) to present": x for x in range(1, 6)}
-        ),
+            choices={f"{x} hour(s) to present": x for x in range(1, 6)})
     ):
         """Pings all the users who sent messages in a period of time.
 
@@ -147,9 +149,13 @@ class Utils(commands.Cog):
         ----------
         hours: The period of time.
         """
-        await inter.send(
-            content=", ".join(
-                f"<@{x}>" for x in self.active_users.get_users_for(3600 * hours)
+        try:
+            logs = self.active_users[channel.id]
+            await inter.send(
+                content=", ".join(
+                    f"<@{x}>" for x in logs.get_users_for(3600 * hours)
+                )
+                or "No users recorded yet, perhaps, it's because the chat has been dead for 5 hours? Or maybe the bot was down."
             )
-            or "No users recorded yet, perhaps, it's because the chat has been dead for 5 hours?"
-        )
+        except KeyError:
+            await inter.send(content="No records for this channel yet.")
